8f91190c1c285a7d1c32e76d0d09a4391102f2c4
2chainzbear <jameswilcock8@gmail.com>
2023-05-30 17:55:34 -0700

    added 3d camera and cubeg

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "raylib.h"
-#include "rlgl.h"
 
 #define PRINT_VEC2(var) printf("%s = {%f, %f}\n", #var, var.x, var.y);
 
@@ -9,9 +8,21 @@ int main(void) {
   const int window_width = 800;
   const int window_height = 600;
   InitWindow(window_width, window_height, "Shaders");
-  // SetTargetFPS(60);
+  SetTargetFPS(60);
 
   RenderTexture2D target = LoadRenderTexture(GetScreenWidth(), GetScreenHeight());
+  Mesh cube = GenMeshCube(1,1,1);
+  Material defaultmat = LoadMaterialDefault();
+  Model cube_model = LoadModelFromMesh(cube);
+  Vector3 cube_position = { 0.0f, 0.0f, 0.0f };            // Set model position
+
+  Camera camera = { 0 };
+  camera.position = (Vector3){ 2.0f, 3.0f, 2.0f };    // Camera position  
+  camera.target = (Vector3){ 0.0f, 1.0f, 0.0f };      // Camera looking at point
+  camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+  camera.fovy = 45.0f;                                // Camera field-of-view Y
+  camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
+
 
   const char *fragShaderFileName = "./shader.fs";
   long fragShaderFileModTime = GetFileModTime(fragShaderFileName);
@@ -27,7 +38,7 @@ int main(void) {
   SetShaderValue(shader, resolutionLoc, &resolution, SHADER_UNIFORM_VEC2);
 
   while (!WindowShouldClose()) {
-
+    UpdateCamera(&camera, CAMERA_ORBITAL);
     Vector2 mousepos = GetMousePosition();
     SetShaderValue(shader, mouseposLoc, &mousepos, SHADER_UNIFORM_VEC2);
     float time = (float)GetTime();
@@ -57,10 +68,15 @@ int main(void) {
     EndTextureMode();
 
     BeginDrawing();
+    ClearBackground(RAYWHITE);
     BeginShaderMode(shader);
       DrawTextureEx(target.texture, (Vector2){0, 0}, 0, 1, WHITE);
     EndShaderMode();
     DrawFPS(0, 0);
+      BeginMode3D(camera);
+      DrawModel(cube_model,cube_position,0.1f,WHITE);
+      DrawGrid(10,1.0f);
+      EndMode3D();
     EndDrawing();
 
   }
